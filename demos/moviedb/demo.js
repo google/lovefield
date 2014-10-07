23c41a2082dcc93dc56bf6a345e7c9ef53c7ec81
Arthur Hsu <arthurhsu@chromium.org>
2014-10-07 15:34:30 -0700

    Rollback
    "Reduce demo data set to contain 500 movies and its related data only.
    
    This is done so to make the repo smaller and to improve demo data
    population speed."
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=77216032

--- demos/moviedb/demo.js
+++ demos/moviedb/demo.js
@@ -68,10 +68,13 @@ function clearDb() {
  */
 function addSampleData() {
   return Promise.all([
-    insertPersonData('actor.json', db.getSchema().getActor()),
+    insertPersonData('actor1.json', db.getSchema().getActor()),
+    insertPersonData('actor2.json', db.getSchema().getActor()),
+    insertPersonData('actor3.json', db.getSchema().getActor()),
     insertPersonData('director.json', db.getSchema().getDirector()),
     insertData('movie.json', db.getSchema().getMovie()),
-    insertData('movieactor.json', db.getSchema().getMovieActor()),
+    insertData('movieactor1.json', db.getSchema().getMovieActor()),
+    insertData('movieactor2.json', db.getSchema().getMovieActor()),
     insertData('moviedirector.json', db.getSchema().getMovieDirector()),
     insertData('moviegenre.json', db.getSchema().getMovieGenre())
   ]);
@@ -97,6 +100,18 @@ function insertData(filename, tableSchema) {
 
 
 /**
+ * @param {number} date Date in YYYYMMDD number format.
+ * @return {!Date}
+ */
+function convertDate(date) {
+  var year = Math.round(date / 10000);
+  var month = Math.round((date / 100) % 100 - 1);
+  var day = Math.round(date % 100);
+  return new Date(year, month, day);
+}
+
+
+/**
  * Inserts data in the database.
  * @param {string} filename The name of the file holding JSON data.
  * @param {!movie.db.schema.Actor|!movie.db.schema.Director} tableSchema The
@@ -107,9 +122,9 @@ function insertPersonData(filename, tableSchema) {
   return getSampleData(filename).then(
       function(data) {
         var rows = data.map(function(obj) {
-          obj.dateOfBirth = new Date(obj.dateOfBirth);
+          obj.dateOfBirth = convertDate(obj.dateOfBirth);
           obj.dateOfDeath = !goog.isNull(obj.dateOfDeath) ?
-              new Date(obj.dateOfDeath) : null;
+              convertDate(obj.dateOfDeath) : null;
           return tableSchema.createRow(obj);
         });
         return db.insert().into(tableSchema).values(rows).exec();
