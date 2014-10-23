0e1908e4683697ee77cf66edb8e023904cc70653
arthurhsu <arthurhsu@google.com>
2014-10-23 11:12:49 -0700

    Add CRUD test for bundled mode database. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=78364418

---
+++ testing/hr_bdb_observer.js
@@ -0,0 +1,23 @@
+goog.provide('hr.bdb.Observer');
+
+goog.require('lf.Observer');
+
+
+
+/**
+ * @implements {lf.Observer}
+ * @struct
+ * @constructor
+ */
+hr.bdb.Observer = function() {
+};
+
+
+/** @override */
+hr.bdb.Observer.prototype.start = function(callback) {
+};
+
+
+/** @override */
+hr.bdb.Observer.prototype.stop = function() {
+};
