3646f39c1c76e20ad5bb08d54d8fbffebd42f0a6
arthurhsu <arthurhsu@google.com>
2015-01-29 12:21:45 -0800

    Move lf.index.KeyRange.getComparator to lf.index.Comparator interface. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=85089806

--- dist/lovefield.js
+++ dist/lovefield.js
@@ -24107,6 +24107,14 @@ lf.index.Comparator.prototype.compare;
 
 
 /**
+ * @param {!lf.index.Index.Key|!Array<(string|number)>} key
+ * @param {!lf.index.KeyRange} range
+ * @return {boolean}
+ */
+lf.index.Comparator.prototype.isInRange;
+
+
+/**
  * @typedef {!function((lf.index.Index.Key|!Array<(string|number)>),
  *                     (lf.index.Index.Key|!Array<(string|number)>)):
  *                     !lf.index.FAVOR}
@@ -25161,6 +25169,159 @@ lf.index.BTreeNode_.deserialize = function(rows, tree) {
 
 /**
  * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @fileoverview Provides default comparators. The comparators in this file
+ * use the following logic for compare: whoever has a bigger array index wins.
+ * For example, DESC order for (3, 5), since 3 shall have a bigger array index
+ * when sorted descending, it wins the comparison.
+ */
+goog.provide('lf.index.MultiKeyComparator');
+goog.provide('lf.index.SimpleComparator');
+
+goog.require('lf.Order');
+goog.require('lf.index');
+goog.require('lf.index.Comparator');
+
+
+
+/**
+ * @implements {lf.index.Comparator}
+ * @constructor
+ *
+ * @param {!lf.Order} order
+ */
+lf.index.SimpleComparator = function(order) {
+  /** @private {!Function} */
+  this.compare_ = (order == lf.Order.DESC) ?
+      lf.index.SimpleComparator.compareDescending :
+      lf.index.SimpleComparator.compareAscending;
+};
+
+
+/**
+ * @param {(string|number)} lhs
+ * @param {(string|number)} rhs
+ * @return {number} -1: favor rhs, 0: equal, 1: favor lhs
+ */
+lf.index.SimpleComparator.compareAscending = function(lhs, rhs) {
+  return lhs > rhs ?
+      lf.index.FAVOR.LHS :
+      (lhs < rhs ? lf.index.FAVOR.RHS : lf.index.FAVOR.TIE);
+};
+
+
+/**
+ * @param {(string|number)} lhs
+ * @param {(string|number)} rhs
+ * @return {number} -1: favor rhs, 0: equal, 1: favor lhs
+ */
+lf.index.SimpleComparator.compareDescending = function(lhs, rhs) {
+  return lhs > rhs ?
+      lf.index.FAVOR.RHS :
+      (lhs < rhs ? lf.index.FAVOR.LHS : lf.index.FAVOR.TIE);
+};
+
+
+/**
+ * @param {!lf.index.Comparator.FunctionType} fn
+ * @param {!lf.index.Index.Key|!Array<(string|number)>} key
+ * @param {!lf.index.KeyRange} range
+ * @return {boolean}
+ */
+lf.index.SimpleComparator.isInRange = function(fn, key, range) {
+  var lowerBoundCheck = goog.isNull(range.from);
+  if (!lowerBoundCheck) {
+    var favor = fn(key, /** @type {!lf.index.Index.Key} */ (range.from));
+    lowerBoundCheck = range.excludeLower ?
+        favor == lf.index.FAVOR.LHS :
+        favor != lf.index.FAVOR.RHS;
+  }
+
+  var upperBoundCheck = goog.isNull(range.to);
+  if (!upperBoundCheck) {
+    var favor = fn(key, /** @type {!lf.index.Index.Key} */ (range.to));
+    upperBoundCheck = range.excludeUpper ?
+        favor == lf.index.FAVOR.RHS :
+        favor != lf.index.FAVOR.LHS;
+  }
+
+  return lowerBoundCheck && upperBoundCheck;
+};
+
+
+/** @override */
+lf.index.SimpleComparator.prototype.compare = function(lhs, rhs) {
+  return this.compare_(lhs, rhs);
+};
+
+
+/** @override */
+lf.index.SimpleComparator.prototype.isInRange = function(key, range) {
+  return lf.index.SimpleComparator.isInRange(this.compare_, key, range);
+};
+
+
+
+/**
+ * @implements {lf.index.Comparator}
+ * @constructor
+ *
+ * @param {!Array<!lf.Order>} orders
+ */
+lf.index.MultiKeyComparator = function(orders) {
+  /** @private {!Array<!Function>} */
+  this.comparators_ = orders.map(function(order) {
+    return order == lf.Order.DESC ?
+        lf.index.SimpleComparator.compareDescending :
+        lf.index.SimpleComparator.compareAscending;
+  });
+};
+
+
+/**
+ * @param {number} numKeys
+ * @param {!lf.Order} order
+ * @return {!Array<!lf.Order>}
+ */
+lf.index.MultiKeyComparator.createOrders = function(numKeys, order) {
+  var orders = new Array(numKeys);
+  for (var i = 0; i < numKeys; ++i) {
+    orders[i] = order;
+  }
+  return orders;
+};
+
+
+/** @override */
+lf.index.MultiKeyComparator.prototype.compare = function(lhs, rhs) {
+  var base = 0;
+  for (var i = 0; i < this.comparators_.length && base == 0; ++i) {
+    base = this.comparators_[i](lhs[i], rhs[i]);
+  }
+  return base;
+};
+
+
+/** @override */
+lf.index.MultiKeyComparator.prototype.isInRange = function(key, range) {
+  // TODO(arthurhsu): implement
+};
+
+/**
+ * @license
  * Copyright 2014 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -25208,30 +25369,6 @@ lf.index.KeyRange = function(from, to, excludeLower, excludeUpper) {
 
   /** @type {boolean} */
   this.excludeUpper = !goog.isNull(this.to) ? excludeUpper : false;
-};
-
-
-/**
- * @return {!function(!lf.index.Index.Key):boolean} A comparator function that
- *     checks whether a key resides within the speficied key range.
- */
-lf.index.KeyRange.prototype.getComparator = function() {
-  var lowerBoundComparator = goog.isNull(this.from) ?
-      function() { return true; } :
-      this.excludeLower ?
-          goog.bind(function(key) { return key > this.from; }, this) :
-          goog.bind(function(key) { return key >= this.from; }, this);
-
-  var upperBoundComparator = goog.isNull(this.to) ?
-      function() { return true; } :
-      this.excludeUpper ?
-          goog.bind(function(key) { return key < this.to; }, this) :
-          goog.bind(function(key) { return key <= this.to; }, this);
-
-  return goog.bind(
-      function(key) {
-        return lowerBoundComparator(key) && upperBoundComparator(key);
-      }, this);
 };
 
 
@@ -25344,10 +25481,12 @@ goog.provide('lf.index.RowId');
 
 goog.require('goog.structs.Set');
 goog.require('lf.Exception');
+goog.require('lf.Order');
 goog.require('lf.Row');
 goog.require('lf.index');
 goog.require('lf.index.Index');
 goog.require('lf.index.KeyRange');
+goog.require('lf.index.SimpleComparator');
 
 
 
@@ -25367,6 +25506,9 @@ lf.index.RowId = function(name) {
 
   /** @private {!goog.structs.Set.<!lf.index.Index.Key>} */
   this.rows_ = new goog.structs.Set();
+
+  /** @private {!lf.index.Comparator} */
+  this.comparator_ = new lf.index.SimpleComparator(lf.Order.ASC);
 };
 
 
@@ -25428,9 +25570,11 @@ lf.index.RowId.prototype.getRange = function(
       (goog.isDefAndNotNull(keyRange.to) && typeof(keyRange.to) != 'number')) {
     throw new lf.Exception(lf.Exception.Type.DATA, 'Row id must be numbers');
   }
-  return lf.index.slice(
-      this.rows_.getValues().filter(keyRange.getComparator()),
-      opt_order, opt_limit, opt_skip);
+
+  var values = this.rows_.getValues().filter(function(value) {
+    return this.comparator_.isInRange(value, keyRange);
+  }, this);
+  return lf.index.slice(values, opt_order, opt_limit, opt_skip);
 };
 
 
@@ -25698,9 +25842,11 @@ goog.provide('lf.index.AATree');
 
 goog.require('goog.asserts');
 goog.require('lf.Exception');
+goog.require('lf.Order');
 goog.require('lf.index');
 goog.require('lf.index.Index');
 goog.require('lf.index.KeyRange');
+goog.require('lf.index.SimpleComparator');
 
 
 
@@ -25765,6 +25911,9 @@ lf.index.AATree = function(name) {
 
   /** @private {!lf.index.AANode_} */
   this.root_ = this.nullNode_;
+
+  /** @private {!lf.index.Comparator} */
+  this.comparator_ = new lf.index.SimpleComparator(lf.Order.ASC);
 };
 
 
@@ -25975,7 +26124,7 @@ lf.index.AATree.prototype.traverse_ = function(node, keyRange, results) {
     this.traverse_(node.left, keyRange, results);
   }
 
-  if (keyRange.getComparator()(node.key)) {
+  if (this.comparator_.isInRange(node.key, keyRange)) {
     results.push(node.value);
   }
 
@@ -26063,120 +26212,6 @@ lf.index.AATree.prototype.toString = function() {
 
 /**
  * @license
- * Copyright 2015 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @fileoverview Provides default comparators. The comparators in this file
- * use the following logic for compare: whoever has a bigger array index wins.
- * For example, DESC order for (3, 5), since 3 shall have a bigger array index
- * when sorted descending, it wins the comparison.
- */
-goog.provide('lf.index.MultiKeyComparator');
-goog.provide('lf.index.SimpleComparator');
-
-goog.require('lf.Order');
-goog.require('lf.index');
-goog.require('lf.index.Comparator');
-
-
-
-/**
- * @implements {lf.index.Comparator}
- * @constructor
- *
- * @param {!lf.Order} order
- */
-lf.index.SimpleComparator = function(order) {
-  /** @private {!Function} */
-  this.compare_ = (order == lf.Order.DESC) ?
-      lf.index.SimpleComparator.compareDescending :
-      lf.index.SimpleComparator.compareAscending;
-};
-
-
-/**
- * @param {(string|number)} lhs
- * @param {(string|number)} rhs
- * @return {number} -1: favor rhs, 0: equal, 1: favor lhs
- */
-lf.index.SimpleComparator.compareAscending = function(lhs, rhs) {
-  return lhs > rhs ?
-      lf.index.FAVOR.LHS :
-      (lhs < rhs ? lf.index.FAVOR.RHS : lf.index.FAVOR.TIE);
-};
-
-
-/**
- * @param {(string|number)} lhs
- * @param {(string|number)} rhs
- * @return {number} -1: favor rhs, 0: equal, 1: favor lhs
- */
-lf.index.SimpleComparator.compareDescending = function(lhs, rhs) {
-  return lhs > rhs ?
-      lf.index.FAVOR.RHS :
-      (lhs < rhs ? lf.index.FAVOR.LHS : lf.index.FAVOR.TIE);
-};
-
-
-/** @override */
-lf.index.SimpleComparator.prototype.compare = function(lhs, rhs) {
-  return this.compare_(lhs, rhs);
-};
-
-
-
-/**
- * @implements {lf.index.Comparator}
- * @constructor
- *
- * @param {!Array<!lf.Order>} orders
- */
-lf.index.MultiKeyComparator = function(orders) {
-  /** @private {!Array<!Function>} */
-  this.comparators_ = orders.map(function(order) {
-    return order == lf.Order.DESC ?
-        lf.index.SimpleComparator.compareDescending :
-        lf.index.SimpleComparator.compareAscending;
-  });
-};
-
-
-/**
- * @param {number} numKeys
- * @param {!lf.Order} order
- * @return {!Array<!lf.Order>}
- */
-lf.index.MultiKeyComparator.createOrders = function(numKeys, order) {
-  var orders = new Array(numKeys);
-  for (var i = 0; i < numKeys; ++i) {
-    orders[i] = order;
-  }
-  return orders;
-};
-
-
-/** @override */
-lf.index.MultiKeyComparator.prototype.compare = function(lhs, rhs) {
-  var base = 0;
-  for (var i = 0; i < this.comparators_.length && base == 0; ++i) {
-    base = this.comparators_[i](lhs[i], rhs[i]);
-  }
-  return base;
-};
-
-/**
- * @license
  * Copyright 2014 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -26255,9 +26290,11 @@ goog.provide('lf.index.Map');
 goog.require('goog.asserts');
 goog.require('goog.structs.Map');
 goog.require('goog.structs.Set');
+goog.require('lf.Order');
 goog.require('lf.index');
 goog.require('lf.index.Index');
 goog.require('lf.index.KeyRange');
+goog.require('lf.index.SimpleComparator');
 
 
 
@@ -26276,6 +26313,9 @@ lf.index.Map = function(name) {
    * @private {!goog.structs.Map.<!lf.index.Index.Key,
    *     !goog.structs.Set.<number>>} */
   this.map_ = new goog.structs.Map();
+
+  /** @private {!lf.index.Comparator} */
+  this.comparator_ = new lf.index.SimpleComparator(lf.Order.ASC);
 };
 
 
@@ -26336,10 +26376,8 @@ lf.index.Map.prototype.getRange = function(
   var results = [];
 
   var keyRange = opt_keyRange || lf.index.KeyRange.all();
-  var comparator = keyRange.getComparator();
-
   this.map_.getKeys().sort().forEach(function(key) {
-    if (comparator(key)) {
+    if (this.comparator_.isInRange(key, keyRange)) {
       results = results.concat(this.get(key));
     }
   }, this);
