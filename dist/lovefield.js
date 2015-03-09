7cb8fa498aedbb05c75ab3f1bd087ae342771bbe
dpapad <dpapad@google.com>
2015-03-09 15:05:37 -0700

    Implementing separate GroupByStep.
    This is in preparation of splitting the GROUP BY functionality outside
    of the ProjectionStep.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=88156520

--- dist/lovefield.js
+++ dist/lovefield.js
@@ -39734,6 +39734,91 @@ lf.proc.Database.prototype.close = function() {
 
 /**
  * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+goog.provide('lf.proc.GroupByStep');
+
+goog.require('goog.labs.structs.Multimap');
+goog.require('lf.proc.PhysicalQueryPlanNode');
+goog.require('lf.proc.Relation');
+
+
+
+/**
+ * @constructor @struct
+ * @extends {lf.proc.PhysicalQueryPlanNode}
+ *
+ * @param {!Array<!lf.schema.Column>} groupByColumns
+ */
+lf.proc.GroupByStep = function(groupByColumns) {
+  lf.proc.GroupByStep.base(this, 'constructor',
+      lf.proc.PhysicalQueryPlanNode.ExecType.FIRST_CHILD,
+      lf.proc.PhysicalQueryPlanNode.InputRelationType.SINGLE);
+
+  /** @private {!Array<!lf.schema.Column>} */
+  this.groupByColumns_ = groupByColumns;
+};
+goog.inherits(lf.proc.GroupByStep, lf.proc.PhysicalQueryPlanNode);
+
+
+/** @override */
+lf.proc.GroupByStep.prototype.toString = function() {
+  var columnNames = this.groupByColumns_.map(
+      function(column) {
+        return column.getNormalizedName();
+      });
+  return 'groupBy(' + columnNames.toString() + ')';
+};
+
+
+/** @override */
+lf.proc.GroupByStep.prototype.execInternal = function(journal, relation) {
+  return this.calculateGroupedRelations_(
+      /** @type {!lf.proc.Relation} */ (relation));
+};
+
+
+/**
+ * Breaks down a single relation to mulitple relations by grouping rows based on
+ * the specified groupBy columns.
+ * @param {!lf.proc.Relation} relation
+ * @return {!Array<!lf.proc.Relation>}
+ * @private
+ */
+lf.proc.GroupByStep.prototype.calculateGroupedRelations_ = function(relation) {
+  var groupMap = new goog.labs.structs.Multimap();
+
+  var getKey = function(entry) {
+    var keys = this.groupByColumns_.map(function(column) {
+      return entry.getField(column);
+    }, this);
+    return keys.join(',');
+  }.bind(this);
+
+  relation.entries.forEach(function(entry) {
+    groupMap.add(getKey(entry), entry);
+  }, this);
+
+  return groupMap.getKeys().map(
+      function(key) {
+        return new lf.proc.Relation(groupMap.get(key), relation.getTables());
+      }, this);
+};
+
+/**
+ * @license
  * Copyright 2014 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
---
+++ tests/proc/group_by_step_test.js
@@ -0,0 +1,105 @@
+/**
+ * @license
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+goog.setTestOnly();
+goog.require('goog.testing.jsunit');
+goog.require('hr.db');
+goog.require('lf.cache.Journal');
+goog.require('lf.proc.GroupByStep');
+goog.require('lf.proc.Relation');
+goog.require('lf.schema.DataStoreType');
+goog.require('lf.testing.hrSchema.MockDataGenerator');
+goog.require('lf.testing.proc.DummyStep');
+
+
+/** @type {!hr.db.schema.Employee} */
+var e;
+
+
+/** @type {!hr.db.schema.Job} */
+var j;
+
+
+/** @type {!lf.testing.hrSchema.MockDataGenerator} */
+var dataGenerator;
+
+
+function setUp() {
+  var schema = hr.db.getSchema();
+  j = schema.getJob();
+  e = schema.getEmployee();
+  dataGenerator = new lf.testing.hrSchema.MockDataGenerator(schema);
+  dataGenerator.generate(20, 100, 0);
+  return hr.db.connect({storeType: lf.schema.DataStoreType.MEMORY});
+}
+
+
+/**
+ * Tests GroupByStep#exec() method for the case where grouping is performed on a
+ * single column.
+ * @return {!IThenable}
+ */
+function testExec_SingleColumn() {
+  var inputRelation = lf.proc.Relation.fromRows(
+      dataGenerator.sampleEmployees, [e.getName()]);
+  var childStep = new lf.testing.proc.DummyStep(inputRelation);
+  var groupByStep = new lf.proc.GroupByStep([e.jobId]);
+  groupByStep.addChild(childStep);
+
+  var employeesPerJob = dataGenerator.employeeGroundTruth.employeesPerJob;
+  var journal = new lf.cache.Journal(hr.db.getGlobal(), []);
+  return groupByStep.exec(journal).then(function(relations) {
+    var jobIds = employeesPerJob.getKeys();
+    assertEquals(jobIds.length, relations.length);
+    relations.forEach(function(relation) {
+      var jobId = relation.entries[0].getField(e.jobId);
+      var expectedRows = employeesPerJob.get(jobId);
+      assertEquals(expectedRows.length, relation.entries.length);
+      relation.entries.forEach(function(entry) {
+        assertEquals(jobId, entry.getField(e.jobId));
+      });
+    });
+  });
+}
+
+
+/**
+ * Tests GroupByStep#exec() method for the case where grouping is performed on
+ * multiple columns.
+ * @return {!IThenable}
+ */
+function testExec_MultiColumn() {
+  var inputRelation = lf.proc.Relation.fromRows(
+      dataGenerator.sampleJobs, [j.getName()]);
+  var childStep = new lf.testing.proc.DummyStep(inputRelation);
+  var groupByStep = new lf.proc.GroupByStep([j.minSalary, j.maxSalary]);
+  groupByStep.addChild(childStep);
+
+  var journal = new lf.cache.Journal(hr.db.getGlobal(), []);
+  return groupByStep.exec(journal).then(function(relations) {
+    var jobCount = 0;
+    relations.forEach(function(relation) {
+      var groupByMinSalary = relation.entries[0].getField(j.minSalary);
+      var groupByMaxSalary = relation.entries[0].getField(j.maxSalary);
+      relation.entries.forEach(function(entry) {
+        assertEquals(groupByMinSalary, entry.getField(j.minSalary));
+        assertEquals(groupByMaxSalary, entry.getField(j.maxSalary));
+        jobCount++;
+      });
+    });
+    assertEquals(inputRelation.entries.length, jobCount);
+  });
+}
