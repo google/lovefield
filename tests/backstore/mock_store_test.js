abe23681aeac527dd8f1b41ab71ea6d57cdffc58
dpapad <dpapad@google.com>
2015-03-23 10:23:41 -0700

    Implementing lf.testing.backstore.MockStore to simulate external changes.
    An external change is a modification of the backing store that has already
    occurred
     - via a different tab/window for the case of a local DB (like IndexedDB), or
     - via a different client for the case of a remote DB (like Firebase).
    
    MockStore is a wrapper around the actual backstore (the one registered in
    lf.Global). MockStore itself is not registered in lf.Global. Changes submitted
    through MockStore will result in
     1) Modifying the actual backstore data.
     2) Notifying observers of the actual backstore, giving them a chance to
        update Lovefield's internal state (in-memory indices/caches) accordingly.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=89311971

---
+++ tests/backstore/mock_store_test.js
@@ -0,0 +1,155 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+goog.setTestOnly();
+goog.require('goog.testing.AsyncTestCase');
+goog.require('goog.testing.jsunit');
+goog.require('lf.Global');
+goog.require('lf.TransactionType');
+goog.require('lf.cache.DefaultCache');
+goog.require('lf.cache.Journal');
+goog.require('lf.index.MemoryIndexStore');
+goog.require('lf.service');
+goog.require('lf.testing.MockSchema');
+goog.require('lf.testing.backstore.MockStore');
+goog.require('lf.testing.backstore.ObservableStore');
+goog.require('lf.testing.backstore.ScudTester');
+
+
+/** @type {!goog.testing.AsyncTestCase} */
+var asyncTestCase = goog.testing.AsyncTestCase.createAndInstall(
+    'MockStoreTest');
+
+
+/** @type {!lf.testing.backstore.ObservableStore} */
+var actualStore;
+
+
+/** @type {!lf.testing.backstore.MockStore} */
+var mockStore;
+
+
+/** @type {!lf.cache.Cache} */
+var cache;
+
+
+/** @type {!lf.schema.Database} */
+var schema;
+
+
+function setUp() {
+  asyncTestCase.waitForAsync('setUp');
+
+  cache = new lf.cache.DefaultCache();
+  var indexStore = new lf.index.MemoryIndexStore();
+  schema = new lf.testing.MockSchema();
+
+  var global = lf.Global.get();
+  global.registerService(lf.service.CACHE, cache);
+  global.registerService(lf.service.INDEX_STORE, indexStore);
+  global.registerService(lf.service.SCHEMA, schema);
+
+  actualStore = new lf.testing.backstore.ObservableStore(schema);
+  mockStore = new lf.testing.backstore.MockStore(actualStore);
+
+  mockStore.init().then(function() {
+    asyncTestCase.continueTesting();
+  }, fail);
+}
+
+
+/**
+ * Tests that the testing.backstore.MockStore is instantiated according to the
+ * schema instance that is passed into its constructor.
+ */
+function testConstruction() {
+  assertTrue(schema.tables().length > 0);
+
+  schema.tables().forEach(
+      function(table) {
+        assertNotNull(mockStore.getTableInternal(table.getName()));
+      });
+}
+
+
+function testGetTable_NonExisting() {
+  assertThrows(function() {
+    mockStore.getTableInternal('nonExistingTableName');
+  });
+}
+
+
+function testSCUD() {
+  var scudTester = new lf.testing.backstore.ScudTester(
+      mockStore, lf.Global.get());
+
+  scudTester.run().then(function() {
+    asyncTestCase.continueTesting();
+  });
+
+  asyncTestCase.waitForAsync('testSCUD');
+}
+
+
+/**
+ * Tests that when a backstore change is submitted via the MockStore interface,
+ * observers of the actual backstore (the one registered in lf.Global) are
+ * notified.
+ */
+function testSimulateExternalChange() {
+  asyncTestCase.waitForAsync('testSimulateExternalChange');
+
+  var tableSchema = schema.table('tableA');
+  var rows = new Array(10);
+  for (var i = 0; i < rows.length; i++) {
+    rows[i] = tableSchema.createRow({
+      'id': 'id' + i.toString(),
+      'name': 'name' + i.toString()
+    });
+  }
+
+  // Adding an observer in the actual backstore (the one that is registered in
+  // lf.Global).
+  actualStore.subscribe(function(tableDiffs) {
+    assertEquals(1, tableDiffs.length);
+    assertEquals(tableSchema.getName(), tableDiffs[0].getName());
+    assertEquals(5, tableDiffs[0].getAdded().getCount());
+    assertEquals(0, tableDiffs[0].getModified().getCount());
+    assertEquals(0, tableDiffs[0].getDeleted().getCount());
+
+    asyncTestCase.continueTesting();
+  });
+
+  // Using the MockStore to simulate an external backstore change. Changes that
+  // are triggered via the MockStore should result in events firing on the
+  // actual backing store observers.
+  var tx = mockStore.createTx(
+      lf.TransactionType.READ_WRITE,
+      new lf.cache.Journal(lf.Global.get(), [tableSchema]));
+  var table = tx.getTable(
+      tableSchema.getName(), tableSchema.deserializeRow.bind(tableSchema));
+
+  // Insert 10 rows.
+  table.put(rows).then(function() {
+    // Delete the last 5 rows.
+    var rowIds = rows.slice(rows.length / 2).map(function(row) {
+      return row.id();
+    });
+    return table.remove(rowIds);
+  }).then(function() {
+    tx.commit();
+  });
+}
