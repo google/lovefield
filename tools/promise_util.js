aa7dd093e31f0047a659e6387130da92216f9de9
dpapad <dpapad@google.com>
2015-04-08 13:37:43 -0700

    Adding missing files to MOE exported files. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=90642131

---
+++ tools/promise_util.js
@@ -0,0 +1,63 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
+ * @typedef {{
+ *   fn: function(): !IThenable,
+ *   name: string
+ * }}
+ * @private
+ */
+var FunctionItem_;
+
+
+/**
+ * @param {!Array<!FunctionItem_>} functionItems
+ * @param {!function(!FunctionItem_):void=} opt_onStart A function to call right
+ *     before starting executing the next function.
+ * @return {!IThenable}
+ */
+function sequentiallyRun(functionItems, opt_onStart) {
+  var onStart = opt_onStart || null;
+
+  return new Promise(function(resolve, reject) {
+    var results = new Array(functionItems.length);
+    var i = 0;
+
+    var runner = function() {
+      var functionItem = functionItems[i];
+      if (onStart != null) {
+        onStart(functionItem);
+      }
+      functionItem.fn().then(function(result) {
+        results[i] = result;
+        if (i < functionItems.length - 1) {
+          i++;
+          runner();
+        } else {
+          resolve();
+        }
+      }, reject);
+    };
+    runner();
+  });
+}
+
+
+/** @type {!Function} */
+exports.sequentiallyRun = sequentiallyRun;
---
+++ tools/strip_license.js
@@ -0,0 +1,80 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var streamMod = require('stream');
+var utilMod = require('util');
+var bufferMod = require('buffer');
+
+
+
+/**
+ * @constructor
+ *
+ * @param {!Object} options
+ */
+var StripLicense = function(options) {
+  streamMod.Transform.call(this, options);
+};
+utilMod.inherits(StripLicense, streamMod.Transform);
+
+
+/**
+ * @param {{contents: !Object}} file
+ * @param {string} enc
+ * @param {!Function} callback
+ */
+StripLicense.prototype._transform = function(file, enc, callback) {
+  var inputContents = file.contents.toString();
+  var outputContents = this.stripLicense_(inputContents);
+  file.contents = new bufferMod.Buffer(outputContents);
+  callback(null, file);
+};
+
+
+/**
+ * Strips redundant license declarations and keeps only one at the beginning.
+ * @param {string} input The input that contains multiple License declarations.
+ * @return {string}
+ * @private
+ */
+StripLicense.prototype.stripLicense_ = function(input) {
+  var stripped = input.replace(
+      /(?:\/\*(?:[\s\S]*?)\*\/)|(?:([\s;])+\/\/(?:.*)$)/gm, '');
+  var LICENSE = [
+    '/*',
+    '  Copyright 2014 Google Inc. All Rights Reserved.',
+    '',
+    '  Licensed under the Apache License, Version 2.0 (the "License");',
+    '  you may not use this file except in compliance with the License.',
+    '  You may obtain a copy of the License at',
+    '',
+    '  http://www.apache.org/licenses/LICENSE-2.0',
+    '',
+    '  Unless required by applicable law or agreed to in writing, software',
+    '  distributed under the License is distributed on an "AS IS" BASIS,',
+    '  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or' +
+        ' implied.',
+    '  See the License for the specific language governing permissions and',
+    '  limitations under the License.',
+    '*/',
+    ''
+  ].join('\n');
+  return LICENSE + stripped;
+};
+
+
+/** @type {!Function} */
+exports.StripLicense = StripLicense;
