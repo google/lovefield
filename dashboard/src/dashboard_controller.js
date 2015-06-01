bb0ada1156452e4968672f468f8379bbb22954cf
Arthur Hsu <arthurhsu@chromium.org>
2015-06-01 10:58:59 -0700

    Adding Dashboard

---
+++ dashboard/src/dashboard_controller.js
@@ -0,0 +1,151 @@
+/**
+ * @license
+ * Copyright 2015 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+/**
+ * @param {!angular.Scope} $scope
+ * @param {!SyncService} syncService
+ * @param {!LovefieldService} lovefieldService
+ * @constructor
+ */
+var DashboardController = function($scope, syncService, lovefieldService) {
+  this.scope_ = $scope;
+  this.lovefieldService_ = lovefieldService;
+
+  this.testSuites = [];
+
+  this.lovefieldService_.getDbConnection().then(function() {
+    // Draw initially with data from the database.
+    this.redraw_();
+
+    // Get notified when new data has been synced.
+    this.lovefieldService_.whenLastSyncDateChanged(function() {
+      console.log('New data found, redrawing...');
+      this.redraw_();
+    }.bind(this));
+
+    // Spawn server sync.
+    console.log('Syncing with server...');
+    syncService.init();
+
+  }.bind(this));
+
+  this.viewMap_ = buildViewMap();
+};
+
+
+/**
+ * Re-draws the entire dashboard.
+ * @private
+ */
+DashboardController.prototype.redraw_ = function() {
+  this.lovefieldService_.getTestSuiteList().then(function(results) {
+    this.testSuites = results;
+    this.scope_.$apply();
+
+    this.drawGeoMean_();
+
+    // Draw all views (test suites).
+    this.testSuites.forEach(function(testSuite) {
+       this.drawView_(this.viewMap_.get(testSuite));
+    }, this);
+  }.bind(this));
+};
+
+
+/**
+ * Draws the graphs for a given view (test suite).
+ * @param {!Object} view
+ * @private
+ */
+DashboardController.prototype.drawView_ = function(view) {
+  var sectionEl = document.getElementById(view.name);
+
+  view.graphs.forEach(function(graph, index) {
+    var containerEl = sectionEl.getElementsByClassName(
+        'graph-container')[index];
+    this.drawGraph_(view.name, graph, containerEl);
+  }, this);
+};
+
+
+/**
+ * Draws a given graph.
+ * @param {string} testSuiteName
+ * @param {!Object} graph
+ * @param {!HTMLElement} containerEl
+ * @return {!IThenable}
+ * @private
+ */
+DashboardController.prototype.drawGraph_ = function(
+    testSuiteName, graph, containerEl) {
+  DashboardController.clearSvg_(containerEl);
+  var promises = graph.curves.map(
+      function(curveName) {
+        return this.lovefieldService_.getTestCaseDataByName(
+            testSuiteName, curveName);
+      }, this);
+
+  return Promise.all(promises).then(function(results) {
+    var graphPlotter = new GraphPlotter(containerEl);
+    results.forEach(function(result, index) {
+      var curve = new Curve(
+          graph.curves[index],
+          result,
+          function(d) { return d['date']; },
+          function(d) { return d['execTime']; });
+      graphPlotter.addCurve(curve);
+    });
+
+    graphPlotter.draw();
+  });
+};
+
+
+/**
+ * Draws the geometric mean graph.
+ * @private
+ */
+DashboardController.prototype.drawGeoMean_ = function() {
+  var containerEl = document.getElementById('geometric-mean');
+  DashboardController.clearSvg_(containerEl);
+
+  this.lovefieldService_.getGeoMeanData().then(function(results) {
+    var graphPlotter = new GraphPlotter(containerEl);
+    var curve = new Curve(
+        'Daily Geometric Mean',
+        results,
+        function(d) { return d['date']; },
+        function(d) { return d['GEOMEAN(execTime)']; });
+    graphPlotter.addCurve(curve);
+    graphPlotter.draw();
+  }.bind(this));
+};
+
+
+/**
+ * Removes the first SVG found in the given container from the document.
+ * @param {!HTMLElement} containerEl
+ * @private
+ */
+DashboardController.clearSvg_ = function(containerEl) {
+  var previousSvg = containerEl.getElementsByTagName('svg')[0];
+  if (previousSvg) {
+    previousSvg.remove();
+  }
+};
---
+++ dashboard/src/views.js
@@ -0,0 +1,240 @@
+var loadingEmptyDbView = {
+  name: 'Loading_Empty_DB',
+  graphs: [
+    {
+      name: 'Initialize empty db',
+      curves: [
+        'Init empty DB'
+      ]
+    }
+  ]
+};
+
+
+var loadingPopulatedDbView = {
+  name: 'Loading_Populated_DB',
+  graphs: [
+    {
+      name: 'Initialize populated db',
+      curves: [
+        'Init populated DB'
+      ]
+    }
+  ]
+};
+
+
+fullTableScudGraphs = [
+  {
+    name: 'Full table scan: Select',
+    curves: [
+      'Select 10000',
+      'Select 20000',
+      'Select 30000',
+      'Select 40000',
+      'Select 50000'
+    ]
+  },
+  {
+    name: 'Full table scan: Insert',
+    curves: [
+      'Insert 10000',
+      'Insert 20000',
+      'Insert 30000',
+      'Insert 40000',
+      'Insert 50000'
+    ]
+  },
+  {
+    name: 'Full table scan: Update',
+    curves: [
+      'Update 10000',
+      'Update 20000',
+      'Update 30000',
+      'Update 40000',
+      'Update 50000'
+    ]
+  },
+  {
+    name: 'Full table scan: Delete',
+    curves: [
+      'Delete 10000',
+      'Delete 20000',
+      'Delete 30000',
+      'Delete 40000',
+      'Delete 50000'
+    ]
+  }
+];
+
+
+var fullTableScudView = {
+  name: 'Full_table_SCUD',
+  graphs: fullTableScudGraphs
+};
+
+
+var fullTableScudMemView = {
+  name: 'Full_table_SCUD_Mem',
+  graphs: fullTableScudGraphs
+};
+
+
+var pkBasedScudGraphs = [
+  {
+    name: 'PK-based table scan: Select',
+    curves: [
+      'Select 1',
+      'Select 10',
+      'Select 100',
+      'Select 1000',
+      'Select 10000'
+    ]
+  },
+  {
+    name: 'PK-based table scan: Insert',
+    curves: [
+      'Insert 1',
+      'Insert 10',
+      'Insert 100',
+      'Insert 1000',
+      'Insert 10000'
+    ]
+  },
+  {
+    name: 'PK-based table scan: Update',
+    curves: [
+      'Update 1',
+      'Update 10',
+      'Update 100',
+      'Update 1000',
+      'Update 10000'
+    ]
+  },
+  {
+    name: 'PK-based table scan: Delete',
+    curves: [
+      'Delete 1',
+      'Delete 10',
+      'Delete 100',
+      'Delete 1000',
+      'Delete 10000'
+    ]
+  }
+];
+
+
+var pkBasedScudView = {
+  name: 'PK-based_SCUD',
+  graphs: pkBasedScudGraphs
+};
+
+
+var pkBasedScudMemView = {
+  name: 'PK-based_SCUD_Mem',
+  graphs: pkBasedScudGraphs
+};
+
+
+var selectBenchmarkGraphs = [
+  {
+    name: 'Select single with predicates',
+    curves: [
+      'SelectSingleRowIndexed',
+      'SelectSingleRowNonIndexed',
+      'SelectSingleRowMultipleIndices'
+    ]
+  },
+  {
+    name: 'Select many with predicates',
+    curves: [
+      'SelectMultiRowIndexedRange',
+      'SelectMultiRowIndexedSpacedOut',
+      'SelectMultiRowNonIndexedRange',
+      'SelectMultiRowNonIndexedSpacedOut'
+    ]
+  },
+  {
+    name: 'Select all with projections',
+    curves: [
+      'SelectProjectNonAggregatedColumns',
+      'SelectProjectAggregateIndexed',
+      'SelectProjectAggregateNonIndexed'
+    ]
+  },
+  {
+    name: 'Select all with ordering',
+    curves: [
+      'SelectOrderByIndexed',
+      'SelectOrderByNonIndexed',
+      'SelectOrderByIndexedCrossColumn',
+      'SelectLimitSkipIndexed'
+    ]
+  },
+  {
+    name: 'Select all with join operations',
+    curves: [
+      'SelectJoinEqui',
+      'SelectJoinTheta'
+    ]
+  }
+];
+
+
+var selectBenchmarkView = {
+  name: 'SelectBenchmark',
+  graphs: selectBenchmarkGraphs
+};
+
+
+var selectBenchmarkMemView = {
+  name: 'SelectBenchmark_Mem',
+  graphs: selectBenchmarkGraphs
+};
+
+
+var scenarioSimulationsView = {
+  name: 'Scenario_Simulations',
+  graphs: [
+    {
+      name: 'Insert via tx attach',
+      curves: [
+        'Insert via Tx Attach'
+      ]
+    },
+    {
+      name: 'Select',
+      curves: [
+        'Select'
+      ]
+    },
+    {
+      name: 'Select binding',
+      curves: [
+        'Select Binding'
+      ]
+    }
+  ]
+};
+
+
+function buildViewMap() {
+  var viewMap = new Map();
+  var views = [
+    fullTableScudMemView,
+    fullTableScudView,
+    loadingEmptyDbView,
+    loadingPopulatedDbView,
+    pkBasedScudMemView,
+    pkBasedScudView,
+    scenarioSimulationsView,
+    selectBenchmarkMemView,
+    selectBenchmarkView
+  ];
+
+  views.forEach(function(view) {
+    viewMap.set(view.name, view);
+  });
+
+  return viewMap;
+}
