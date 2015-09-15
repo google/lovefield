27423a1f2ba5fa5ac701f7bd66d7e0d4300c931a
dpapad <dpapad@google.com>
2015-09-14 17:50:58 -0700

    Stop using lf.testing.getSchemaBuilder from context_test.js.
    The reason is that mock schema is used by 20+ test files, and therefore it is
    very hard to change it for the purposes of a single test, without affecting
    other tests. A better approach (followed by this CL) is to have tests create a
    schema on-the-fly, the benefits are
     1) Other tests are not affected when the schema of a single test is modified.
     2) On-the-fly schemas are minimal therefore making it easier to understand
        what is being tested.
    
    This is in preparation of adding more tests for the case of CASCADE
    constraints.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=103050011

--- tests/query/context_test.js
+++ tests/query/context_test.js
@@ -17,151 +17,257 @@
 goog.setTestOnly();
 
 goog.require('goog.testing.jsunit');
+goog.require('lf.Type');
 goog.require('lf.query.DeleteContext');
 goog.require('lf.query.InsertContext');
 goog.require('lf.query.UpdateContext');
-goog.require('lf.testing.getSchemaBuilder');
+goog.require('lf.schema');
 
 
-/** @type {!lf.schema.Database} */
-var schema;
-
-
-function setUpPage() {
-  schema = lf.testing.getSchemaBuilder().getSchema();
+/**
+ * @return {!lf.schema.Database} A schema where TableC refers to TableB, and
+ *     TableB refers to tableA.
+ */
+function getSchemaWithTableChain() {
+  var schemaBuilder = lf.schema.create('contexttest', 1);
+  schemaBuilder.createTable('TableA').
+      addColumn('id', lf.Type.STRING).
+      addPrimaryKey(['id']);
+  schemaBuilder.createTable('TableB').
+      addColumn('id', lf.Type.STRING).
+      addColumn('foreignKey', lf.Type.STRING).
+      addPrimaryKey(['id']).
+      addForeignKey('fk_tableA', {
+        local: 'foreignKey',
+        ref: 'TableA.id'
+      });
+  schemaBuilder.createTable('TableC').
+      addColumn('id', lf.Type.STRING).
+      addColumn('foreignKey', lf.Type.STRING).
+      addForeignKey('fk_tableB', {
+        local: 'foreignKey',
+        ref: 'TableB.id'
+      });
+  return schemaBuilder.getSchema();
 }
+
+
+/**
+ * @return {!lf.schema.Database} A schema where table Child refers to Parent.
+ */
+function getSchemaWithOneForeignKey() {
+  var schemaBuilder = lf.schema.create('contexttest', 1);
+  schemaBuilder.createTable('Child').
+      addColumn('id', lf.Type.STRING).
+      addForeignKey('fk_Id', {
+        local: 'id',
+        ref: 'Parent.id'
+      });
+  schemaBuilder.createTable('Parent').
+      addColumn('id', lf.Type.STRING).
+      addPrimaryKey(['id']);
+  return schemaBuilder.getSchema();
+}
+
+
+/**
+ * @return {!lf.schema.Database} A schema where TableB1 and TableB2 both refer
+ *     to TableA.
+ */
+function getSchemaWithTwoForeignKeys() {
+  var schemaBuilder = lf.schema.create('contexttest', 1);
+  schemaBuilder.createTable('TableA').
+      addColumn('id1', lf.Type.STRING).
+      addColumn('id2', lf.Type.STRING).
+      addUnique('uq_id1', ['id1']).
+      addUnique('uq_id2', ['id2']);
+  schemaBuilder.createTable('TableB1').
+      addColumn('id', lf.Type.STRING).
+      addColumn('foreignKey', lf.Type.STRING).
+      addForeignKey('fk_tableA', {
+        local: 'foreignKey',
+        ref: 'TableA.id1'
+      });
+  schemaBuilder.createTable('TableB2').
+      addColumn('id', lf.Type.STRING).
+      addColumn('foreignKey', lf.Type.STRING).
+      addForeignKey('fk_tableA', {
+        local: 'foreignKey',
+        ref: 'TableA.id2'
+      });
+  return schemaBuilder.getSchema();
+}
+
+
+/**
+ * @return {!lf.schema.Database} A schema where no foreign keys exist.
+ */
+function getSchemaWithoutForeignKeys() {
+  var schemaBuilder = lf.schema.create('contexttest', 1);
+  schemaBuilder.createTable('TableA').
+      addColumn('id', lf.Type.STRING);
+  schemaBuilder.createTable('TableB').
+      addColumn('id', lf.Type.STRING);
+  return schemaBuilder.getSchema();
+}
+
 
 function testGetScope_Insert() {
+  var schema = getSchemaWithOneForeignKey();
   var context = new lf.query.InsertContext(schema);
-  var tableG = schema.table('tableG');
-  var row = tableG.createRow();
+  var childTable = schema.table('Child');
+  var parentTable = schema.table('Parent');
+  var row = childTable.createRow();
   context.values = [row];
-  context.into = tableG;
+  context.into = childTable;
   var scope = context.getScope();
-  assertTrue(scope.has(tableG));
+  assertTrue(scope.has(childTable));
   assertEquals(2, scope.size);
-  assertTrue(scope.has(schema.table('tableI')));
+  assertTrue(scope.has(parentTable));
 }
+
 
 function testGetScope_InsertNoExpansion() {
+  var schema = getSchemaWithoutForeignKeys();
   var context = new lf.query.InsertContext(schema);
-  var tableC = schema.table('tableC');
-  var row = tableC.createRow();
+  var tableA = schema.table('TableA');
+  var row = tableA.createRow();
   context.values = [row];
-  context.into = tableC;
+  context.into = tableA;
   var scope = context.getScope();
-  assertTrue(scope.has(tableC));
+  assertTrue(scope.has(tableA));
   assertEquals(1, scope.size);
 }
+
 
 function testGetScope_InsertOrReplace() {
+  var schema = getSchemaWithTableChain();
   var context = new lf.query.InsertContext(schema);
   context.allowReplace = true;
-  var tableG = schema.table('tableG');
-  var tableI = schema.table('tableI');
-  var tableH = schema.table('tableH');
-  var row = tableG.createRow();
+  var tableA = schema.table('TableA');
+  var tableB = schema.table('TableB');
+  var tableC = schema.table('TableC');
+  var row = tableB.createRow();
   context.values = [row];
-  context.into = tableG;
+  context.into = tableB;
   var scope = context.getScope();
-  assertTrue(scope.has(tableG));
   assertEquals(3, scope.size);
-  assertTrue(scope.has(tableI));
-  assertTrue(scope.has(tableH));
+  assertTrue(scope.has(tableA));
+  assertTrue(scope.has(tableB));
+  assertTrue(scope.has(tableC));
 }
+
 
 function testGetScope_InsertOrReplaceNoExpansion() {
+  var schema = getSchemaWithoutForeignKeys();
   var context = new lf.query.InsertContext(schema);
   context.allowReplace = true;
-  var tableC = schema.table('tableC');
-  var row = tableC.createRow();
+  var tableA = schema.table('TableA');
+  var row = tableA.createRow();
   context.values = [row];
-  context.into = tableC;
+  context.into = tableA;
   var scope = context.getScope();
-  assertTrue(scope.has(tableC));
   assertEquals(1, scope.size);
+  assertTrue(scope.has(tableA));
 }
+
 
 function testGetScope_Delete() {
+  var schema = getSchemaWithOneForeignKey();
   var context = new lf.query.DeleteContext(schema);
-  var tableG = schema.table('tableG');
-  var tableH = schema.table('tableH');
-  context.from = tableG;
+  var parentTable = schema.table('Parent');
+  var childTable = schema.table('Child');
+  context.from = parentTable;
   var scope = context.getScope();
-  assertTrue(scope.has(tableG));
   assertEquals(2, scope.size);
-  assertTrue(scope.has(tableH));
+  assertTrue(scope.has(parentTable));
+  assertTrue(scope.has(childTable));
 }
+
 
 function testGetScope_DeleteNoExpansion() {
+  var schema = getSchemaWithoutForeignKeys();
   var context = new lf.query.DeleteContext(schema);
-  var tableC = schema.table('tableC');
-  context.from = tableC;
+  var tableA = schema.table('TableA');
+  context.from = tableA;
   var scope = context.getScope();
-  assertTrue(scope.has(tableC));
   assertEquals(1, scope.size);
+  assertTrue(scope.has(tableA));
 }
+
 
 function testGetScope_UpdateOneColumn() {
+  var schema = getSchemaWithTwoForeignKeys();
   var context = new lf.query.UpdateContext(schema);
-  var tableI = schema.table('tableI');
-  var tableH = schema.table('tableH');
-  context.table = tableI;
-  context.set = [{column: tableI['id2'], value: 'test'}];
+  var tableA = schema.table('TableA');
+  var tableB1 = schema.table('TableB1');
+  context.table = tableA;
+  context.set = [{column: tableA['id1'], value: 'test1'}];
   var scope = context.getScope();
   assertEquals(2, scope.size);
-  assertTrue(scope.has(tableI));
-  assertTrue(scope.has(tableH));
+  assertTrue(scope.has(tableA));
+  assertTrue(scope.has(tableB1));
 }
+
 
 function testGetScope_UpdateTwoColumns() {
+  var schema = getSchemaWithTwoForeignKeys();
   var context = new lf.query.UpdateContext(schema);
-  var tableG = schema.table('tableG');
-  var tableI = schema.table('tableI');
-  var tableH = schema.table('tableH');
-  context.table = tableI;
-  context.set = [{column: tableI['id2'], value: 'test'},
-                 {column: tableI['id'], value: 'test'}];
+  var tableA = schema.table('TableA');
+  var tableB1 = schema.table('TableB1');
+  var tableB2 = schema.table('TableB2');
+  context.table = tableA;
+  context.set = [
+    {column: tableA['id1'], value: 'test1'},
+    {column: tableA['id2'], value: 'test2'},
+  ];
   var scope = context.getScope();
   assertEquals(3, scope.size);
-  assertTrue(scope.has(tableG));
-  assertTrue(scope.has(tableI));
-  assertTrue(scope.has(tableH));
+  assertTrue(scope.has(tableA));
+  assertTrue(scope.has(tableB1));
+  assertTrue(scope.has(tableB2));
 }
 
+
 function testGetScope_UpdateReferredColumn() {
+  var schema = getSchemaWithTableChain();
   var context = new lf.query.UpdateContext(schema);
-  var tableG = schema.table('tableG');
-  var tableH = schema.table('tableH');
-  context.table = tableG;
-  context.set = [{column: tableG['id2'], value: 'test'}];
+  var tableB = schema.table('TableB');
+  var tableC = schema.table('TableC');
+  context.table = tableB;
+  context.set = [{column: tableB['id'], value: 'test'}];
   var scope = context.getScope();
   assertEquals(2, scope.size);
-  assertTrue(scope.has(tableG));
-  assertTrue(scope.has(tableH));
+  assertTrue(scope.has(tableB));
+  assertTrue(scope.has(tableC));
 }
 
 function testGetScope_UpdateReferredAndReferringColumn() {
+  var schema = getSchemaWithTableChain();
   var context = new lf.query.UpdateContext(schema);
-  var tableG = schema.table('tableG');
-  var tableI = schema.table('tableI');
-  var tableH = schema.table('tableH');
-  context.table = tableG;
-  context.set = [{column: tableG['id2'], value: 'test'},
-                 {column: tableG['id'], value: 'test'}];
+
+  var tableA = schema.table('TableA');
+  var tableB = schema.table('TableB');
+  var tableC = schema.table('TableC');
+  context.table = tableB;
+  context.set = [
+    {column: tableB['id'], value: 'test'},
+    {column: tableB['foreignKey'], value: 'test'}
+  ];
   var scope = context.getScope();
   assertEquals(3, scope.size);
-  assertTrue(scope.has(tableG));
-  assertTrue(scope.has(tableH));
-  assertTrue(scope.has(tableI));
+  assertTrue(scope.has(tableA));
+  assertTrue(scope.has(tableB));
+  assertTrue(scope.has(tableC));
 }
 
 function testGetScope_UpdateNoExpansion() {
+  var schema = getSchemaWithoutForeignKeys();
   var context = new lf.query.UpdateContext(schema);
-  var tableC = schema.table('tableC');
-  context.table = tableC;
-  context.set = [{column: tableC['id'], value: 'test'}];
+  var tableA = schema.table('TableA');
+  context.table = tableA;
+  context.set = [{column: tableA['id'], value: 'test'}];
   var scope = context.getScope();
-  assertTrue(scope.has(tableC));
   assertEquals(1, scope.size);
+  assertTrue(scope.has(tableA));
 }
